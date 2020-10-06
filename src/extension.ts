e9aff93a18e80f6935a6afb9ee9e9c6cc5b6753d
Yannick <yannick.francois@is-prod.fr>
2020-10-06 14:01:50 +0200

    Move function into new file

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,6 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { convertArray } from './convertArray';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerTextEditorCommand(
 		'php-convert-array.convertArray',
@@ -13,27 +10,4 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
-
-function convertArray(editor: vscode.TextEditor): Thenable<boolean> {
-	return editor.edit(editBuilder => {
-	  editor.selections.forEach(selection => {
-		const range = new vscode.Range(selection.start, selection.end);
-		let selectedText = editor.document.getText(range) || '';
-		const matches = selectedText.match(/(?=.*[a-zA-Z]).+/g);
-
-		const start = selectedText?.substring(0, 6);
-		const end = selectedText?.substring(selectedText.length - 1);
-
-		if (!matches || !matches.length || start !== 'array(' || end !== ')') {
-		  return;
-		}
-
-		selectedText = selectedText.replace(start, '[').replace(/.$/,"]");
-
-		editBuilder.replace(selection, selectedText);
-	  });
-	});
-  }
-
